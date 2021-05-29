3a23c1ba58509e4e2d701cc4e9f061eaf287f235
amanchhaparia <amanchhaparia125@gmail.com>
2021-05-29 22:21:17 +0530

    Colorname proto type added

--- CSRT/try1.py
+++ CSRT/try1.py
@@ -1,4 +1,13 @@
-import cv2 
-img = cv2.imread("/home/prathamesh/OpenCV-Trackers/assets/dog_test.png")
-cv2.imshow("chal jaa", img)
-cv2.waitKey(0)
+import h5py
+import numpy as np
+import scipy.io as sio
+import cv2
+
+from im2c import* 
+
+w2c = sio.loadmat('Add the path of w2c.mat ',struct_as_record = False)
+w2c = w2c['w2c']
+
+img = cv2.imread('Add the path')
+
+out = im2c(img,w2c,-1)
