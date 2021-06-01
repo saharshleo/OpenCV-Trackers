93fd84e87a25c7e1abf99dea0c3e530baed7577f
amanchhaparia <amanchhaparia125@gmail.com>
2021-06-01 10:26:32 +0530

    added colornames and channel reliability

--- CSRT/CSRT.py
+++ CSRT/CSRT.py
@@ -1,4 +1,5 @@
 import cv2
+from skimage import feature
 from skimage.feature import hog
 from skimage.transform import resize
 import HOG_features as hogfeat
@@ -32,20 +33,25 @@ def cap_func(element):
 
 class CSRT():
 
-    def __init__(self, frame, roi, debug=True):
-        self.debug = debug
+    def __init__(self, frame, roi, num_features, debug = False):
+        self.debug = None
 
         self.frame = frame
         self.roi = roi
         self.sigma = 100
         self.mu = 5
-        self.beta, self.λ = 3, 0.01
+        self.beta, self.λ, self.n = 3, 0.01, 0.02
         self.g = get_gaussian_map(self.roi, self.sigma)
         
         if self.debug:
             cv2.imshow("Gaussian", self.g)
         
-        self.features = []
+        self.features = [0] * num_features
+        self.h_cap = [0] * num_features # Will contain h_cap values for all channels in sequential order
+        self.p = [] # This variable will store position of object in each frame.
+        self.channel_weights = [0] * num_features # Will store channel weights for all channels.
+        self.G_cap = [0] * num_features # Will store individual G_cap/g_tilda values for channels.
+        self.G_res = 0 # Will store resultant G_cap after using channel_reliability.
 
 
     def set_roiImage(self):
@@ -56,13 +62,12 @@ class CSRT():
 
 
     def generate_features(self, des_orientations, des_pixels_per_cell):
-        # self.features.append(hogfeat.get_hog_features(self.roi_img, des_orientations,
-        #     des_pixels_per_cell)) 
-        self.f = hogfeat.get_hog_features(self.roi_img, des_orientations, des_pixels_per_cell)
-        self.features.append(self.f)
+        for i in range(len(self.features)):
+            self.f = hogfeat.get_hog_features(self.roi_img, des_orientations + i, des_pixels_per_cell + i)
+            self.features[i] = self.f
       
         if self.debug:
-            cv2.imshow('hog_image', self.features[0])
+            cv2.imshow('hog_image', self.features)
 
 
     def get_spatial_reliability_map(self):
@@ -100,62 +105,81 @@ class CSRT():
         
 
     def update_H(self):
-        self.initialize_fgh()
+        # self.initialize_fgh()
         
-        f_hat = cap_func(self.features[0])
-        f_hat_conjugate = np.conjugate(f_hat)
+        for channel_index in range(len(self.features)):
+            f_hat = cap_func(self.features[channel_index])
+            f_hat_conjugate = np.conjugate(f_hat)
         
-        g_hat = cap_func(self.g)
-        g_hat_conjugate = np.conjugate(g_hat)
+            g_hat = cap_func(self.g)
+            g_hat_conjugate = np.conjugate(g_hat)
 
-        self.D = self.h.shape[0] * self.h.shape[1]
+            self.D = self.h.shape[0] * self.h.shape[1]
 
-        self.mu_i = self.mu
-        I_hat = cap_func(self.I)
+            self.mu_i = self.mu
+            I_hat = cap_func(self.I)
 
-        for i in range(4):
-            self.hm = self.h * self.m
-            hm_hat = cap_func(self.hm)
+            for i in range(4):
+                self.hm = self.h * self.m
+                hm_hat = cap_func(self.hm)
 
-            # Eqn 12
-            self.hc = (f_hat * g_hat_conjugate + (self.mu * hm_hat - I_hat)) / (f_hat_conjugate * f_hat + self.mu_i)
+                # Eqn 12
+                self.hc = (f_hat * g_hat_conjugate + (self.mu * hm_hat - I_hat)) / (f_hat_conjugate * f_hat + self.mu_i)
             
-            # Eqn 13
-            self.h = (self.m * np.fft.ifft((I_hat + self.mu * self.hc))) / (self.λ / (2 * self.D) + self.mu_i)
+                # Eqn 13
+                self.h = (self.m * np.fft.ifft((I_hat + self.mu * self.hc))) / (self.λ / (2 * self.D) + self.mu_i)
             
-            hc_cap = cap_func(self.hc)
-            h_cap = cap_func(self.h)
-            # Eqn 11
-            I_hat = I_hat + self.mu * (hc_cap - h_cap)
+                hc_cap = cap_func(self.hc)
+                h_cap = cap_func(self.h)
+                # Eqn 11
+                I_hat = I_hat + self.mu * (hc_cap - h_cap)
             
-            self.mu_i *= self.beta
+                self.mu_i *= self.beta
+            self.h_cap[channel_index] = h_cap
+        print("h_cap for feature channels ", self.h_cap)
+        print("length h_cap", len(self.h_cap))
+        print("Lengths of h_cap", len(h_cap[0]), len(h_cap[1]))
 
 
-    def get_new_roi(self):
-        x, y, w, h = self.roi
+    def calculate_g_cap_and_channel_weights(self):
+        for channel_index in range(len(self.features)):
+            f_hat = cap_func(self.features[channel_index])
+            res = f_hat * cap_func(self.h_cap[channel_index])
+            G = np.fft.ifft2(res)
+            G = (G - G.min()) / (G.max() - G.min())
+            self.G_cap[channel_index] = G
 
-        f_hat = cap_func(self.features[0])
-        res = f_hat * self.h
-        G = np.fft.ifft2(res)
-        G = (G - G.min()) / (G.max() - G.min())
-        
-        _G = np.absolute(G)
-        _G = np.array(_G * 255, dtype=np.dtype('uint8'))
-        if self.debug:
-            cv2.imshow("Output", _G)
-
-        max_value = np.max(G)
-        max_pos = np.where(G == max_value)
-        dy = int(np.mean(max_pos[0]) - G.shape[0] / 2)
-        dx = int(np.mean(max_pos[1]) - G.shape[1] / 2)
+            max_value = np.max(G)
+            G_nms = []
+            for i in range(1, G.shape[0] - 1):
+                for j in range(1, G.shape[1] - 1):
+                    G_nms.append(np.max(G[i - 1 : i + 2, j - 1 : j + 2]))
+            pd_max1_index = G_nms.index(max(G_nms))
+            self.channel_weights[channel_index] = G_nms[pd_max1_index]
+            G_nms.pop(pd_max1_index)
+            self.channel_weights[channel_index] /= max(G_nms)
+            self.channel_weights[channel_index] = 1 / self.channel_weights[channel_index]
+            self.channel_weights[channel_index] = 1 - self.channel_weights[channel_index]
+            self.channel_weights[channel_index] *= max_value
+            self.channel_weights[channel_index] = max(0.5, self.channel_weights[channel_index])
+        sum = 0
+        for channel_index in range(len(self.features)):
+            sum += self.channel_weights[channel_index]
+        for channel_index in range(len(self.features)):
+            self.channel_weights[channel_index] /= sum
+        print("channel_weights", self.channel_weights)
+        print("G_cap", self.G_cap)
 
         if self.debug:
             print("roi", self.roi)
             print('G shape', self.features[0].shape)
-            print('dx, dy',(dx, dy))
 
-        return (x+dx, y+dy, w, h)
-   
+def calculate_final_g_cap(self):
+    for channel_index in range(len(self.features)):
+        self.G_res += self.G_cap[channel_index] * self.channel_weights[channel_index]
+    return self.G_res
 
-    def channel_reliability():
-        pass
+def apply_csrt(self):
+    self.p = self.get_new_roi()
+    # print(self.p)
+    self.channel_reliability()
--- CSRT/test.py
+++ CSRT/test.py
@@ -3,20 +3,22 @@ from skimage.transform import resize
 from CSRT import *
 
 
-img = cv2.imread('/home/saharsh/Desktop/EklavyaLongEdition/MOT/OpenCV-Trackers/assets/dog_test.png')
+img = cv2.imread('/home/prathamesh/OpenCV-Trackers/assets/dog_test.png')
 roi = cv2.selectROI('select roi',img)
 # print(roi)
 # print(type(roi))
 # print(type(img))
 # print(img.shape)
 
-tracker = CSRT(img,roi)
+tracker = CSRT(img, roi, 2)
 tracker.set_roiImage()
 tracker.get_spatial_reliability_map()
-tracker.generate_features(9, 2)
+tracker.generate_features(8, 2)
 tracker.update_H()
+tracker.calculate_g_cap_and_channel_weights()
+# tracker.apply_csrt()
 
-x, y, w, h = tracker.get_new_roi()
-cv2.rectangle(img, (x, y),(x + w, y + h), (0,255,0), 2)
-cv2.imshow('csrt',img)
-cv2.waitKey(0)
+# x, y, w, h = tracker.get_new_roi()
+# cv2.rectangle(img, (x, y),(x + w, y + h), (0,255,0), 2)
+# cv2.imshow('csrt',img)
+# cv2.waitKey(0)
